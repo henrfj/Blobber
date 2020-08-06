10bdacea9ba2b26cf0974abbc5101c2fc01be892
henrfj <h1rik.f@gmail.com>
2020-08-06 11:39:32 +0200

    added 'rigid body' file, as interface with physics

--- blob.js
+++ blob.js
@@ -1,12 +1,18 @@
 
 class Blob{
-    health = 100;
-    posX = 0;
-    posY = 0;
-    velX = 0;
-    velY = 0;
-    raidus = 10;
-    constructor(){
-        
+    color;
+    rigid_body;
+    constructor(color){
+        this.rigid_body = new rigid_body;
+        this.color = color;
+    }
+}
+
+
+class Plant{
+    blob;
+    reproduction_
+    constructor(color){
+        blob = new Blob(color);
     }
 }
---
+++ rigid_body.js
@@ -0,0 +1,12 @@
+
+
+class rigid_body{
+    posX = 0;
+    posY = 0;
+    velX = 0;
+    velY = 0;
+    raidus = 10;
+    constructor(){
+        
+    }
+}
